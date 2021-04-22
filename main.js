0f522115139a383ef18abf7e825f1ec210debb43
Olgerd11 <73791209+Olgerd11@users.noreply.github.com>
2021-04-22 22:17:57 +0300

    linked function with select

--- main.js
+++ main.js
@@ -2,14 +2,21 @@
 
 /* =========== making random letters */
 
-const alfabet = 'abcdefghijklmnopqrstuvwxyz';
-const letters = alfabet.split('');
-const shortArray = [];
-let randomNumber;
-let j = 25;
-const nameList = [];
+const firstOption = document.querySelector('#opt1');
+const secondOption = document.querySelector('#opt2');
+const thirdOption = document.querySelector('#opt3');
+const fourthOption = document.querySelector('#opt4');
+const fifthOption = document.querySelector('#opt5');
+const sixthOption = document.querySelector('#opt6');
 
 const generateLetters = () => {
+	const alfabet = 'abcdefghijklmnopqrstuvwxyz';
+	const letters = alfabet.split('');
+	const shortArray = [];
+	let randomNumber;
+	let j = 25;
+	const nameList = [];
+
 	for (let i = 0; i <= 5; i++) {
 		randomNumber = Math.round(Math.random() * j);
 		shortArray.push(letters[randomNumber]);
@@ -17,16 +24,31 @@ const generateLetters = () => {
 		j--;
 	}
 
-	return shortArray;
+	firstOption.setAttribute('value', shortArray[0]);
+	secondOption.setAttribute('value', shortArray[1]);
+	thirdOption.setAttribute('value', shortArray[2]);
+	fourthOption.setAttribute('value', shortArray[3]);
+	fifthOption.setAttribute('value', shortArray[4]);
+	sixthOption.setAttribute('value', shortArray[5]);
+
+	firstOption.innerText = shortArray[0];
+	secondOption.innerText = shortArray[1];
+	thirdOption.innerText = shortArray[2];
+	fourthOption.innerText = shortArray[3];
+	fifthOption.innerText = shortArray[4];
+	sixthOption.innerText = shortArray[5];
 };
 
-/* ============= working with .json file=========== */
+/* ============= created fetch API =========== */
 
 const requestURL = 'list.json';
 
 async function getJsonData() {
 	const response = await fetch(requestURL);
 	const data = await response.json();
+	console.log(data);
 }
 
-getJsonData();
+/* ============= working with form =========== */
+
+document.addEventListener('DOMContentLoaded', generateLetters());
