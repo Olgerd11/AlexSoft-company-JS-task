64dacd8fa8d8c23b4eba19be607055a345d20d13
Olgerd11 <73791209+Olgerd11@users.noreply.github.com>
2021-04-22 20:33:41 +0300

    added fetch API

--- main.js
+++ main.js
@@ -1,7 +1,32 @@
-let letters;
+'use strict';
+
+/* =========== making random letters */
 
 const alfabet = 'abcdefghijklmnopqrstuvwxyz';
+const letters = alfabet.split('');
+const shortArray = [];
+let randomNumber;
+let j = 25;
+const nameList = [];
 
-letters = alfabet.split('');
+const generateLetters = () => {
+	for (let i = 0; i <= 5; i++) {
+		randomNumber = Math.round(Math.random() * j);
+		shortArray.push(letters[randomNumber]);
+		letters.splice(randomNumber, 1);
+		j--;
+	}
 
-console.log(letters);
+	return shortArray;
+};
+
+/* ============= working with .json file=========== */
+
+const requestURL = 'list.json';
+
+async function getJsonData() {
+	const response = await fetch(requestURL);
+	const data = await response.json();
+}
+
+getJsonData();
