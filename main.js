7127f92985dc96259dcee6ee54f42c4b64e6acc4
Olgerd11 <73791209+Olgerd11@users.noreply.github.com>
2021-04-23 14:31:54 +0300

    finished project

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 'use strict';
-/* =========== making random letters ============ */
+/* =========== general constants ============ */
 
 const allOptions = document.querySelectorAll('option');
 const select = document.querySelector('select');
+
+/* =========== making random letters ============ */
 
 function generateLetters() {
 	const alfabet = 'abcdefghijklmnopqrstuvwxyz';
@@ -30,14 +32,36 @@ function generateLetters() {
 
 document.addEventListener('DOMContentLoaded', generateLetters());
 
-/* ============= created fetch API =========== */
+/* ============= created fetch API request =========== */
 
 async function getJsonData() {
 	const response = await fetch('list.json');
 	const data = await response.json();
-	console.log(this.children);
+
+	document.querySelector('ol').remove();
+	const list = document.createElement('ol');
+	document.querySelector('.container').appendChild(list);
+
+	document.querySelector('p').remove();
+	const paragraph = document.createElement('p');
+	document.querySelector('.container').appendChild(paragraph);
+
+	for (let i of data) {
+		if (this.value === i.name[0].toLowerCase()) {
+			let appendElement = document.createElement('li');
+			document.querySelector('ol').appendChild(appendElement);
+			appendElement.textContent = `${i.name}`;
+		}
+	}
+
+	if (!document.querySelector('li')) {
+		const warning = document.querySelector('p');
+		document.querySelector('.container').appendChild(warning);
+		warning.textContent = 'The list does not contain names beginning with this letter';
+	}
 }
 
-select.addEventListener('focus', getJsonData);
+/* ============ added listeners ============== */
 
+select.addEventListener('focus', getJsonData);
 select.addEventListener('change', getJsonData);
